aab653d554e0ed8cbdf1918334ba019b6e630697
Noah Franco <noah@macbooks-mbp.lan>
2020-11-04 16:19:25 -0500

    added semantic ui for styling

--- src/components/CardFront.js
+++ src/components/CardFront.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import ReactCardFlip from "react-card-flip";
+import { Label } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
 const CardFront = (props) => {
   // props is passing the card object from Card.js component
@@ -12,7 +14,12 @@ const CardFront = (props) => {
   const [hardCount, setHardCount] = useState({hard: 0})
 
 
-  // increment the Esay count
+  // To-do
+  // set a deincrement for the each state
+  // only allow one click per onClick()
+
+
+  // increment the Esay state
   const esayCountFunction = () => {
     let esayClick = esayCount.esay + 1
     setEsayCount({
@@ -20,8 +27,9 @@ const CardFront = (props) => {
     })
     console.log("esayCount: ", esayCount)
   }
+  
 
-  // increment the Medium count
+  // increment the Medium state
   const mediumCountFunction = () => {
     let mediumClick = mediumCount.medium + 1
     setMediumCount({
@@ -30,7 +38,7 @@ const CardFront = (props) => {
     console.log("mediumCount: ", mediumCount)
   }
 
-    // increment the Hard count
+    // increment the Hard state
   const hardCountFunction = () => {
     let hardClick = hardCount.hard + 1
     setHardCount({
@@ -52,7 +60,17 @@ const CardFront = (props) => {
           <p> {props.question} </p>
           <button onClick={(e) => handleClick(e)}> answer </button>
           {/* difficulty Level Votes */}
-          <button onClick={(event) => esayCountFunction(event)}> Esay: {esayCount.esay} </button>
+          <div>
+            <Label onClick={(event) => esayCountFunction(event)} as='a' color='teal'>
+              Esay: {esayCount.esay}
+            </Label>
+            <Label onClick={(event) => mediumCountFunction(event)} as='a' color='yellow' tag>
+              Medium: {mediumCount.medium}
+            </Label>
+            <Label onClick={(event) => hardCountFunction(event)} as='a' color='red' tag>
+              Hard: {hardCount.hard}
+            </Label>
+          </div>
         </div>
         {/* Back of Card */}
         <div className="card">
