ec3df5c90bb7d35a2d65cbb5f1d6791acf339d67
Noah Franco <noahfranco@noahs-mbp.lan>
2020-12-03 12:51:15 -0500

    created sign out functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,18 @@
 import React, {Component} from "react";
 import {Input, Menu, Segment} from 'semantic-ui-react';
 import {Link} from "react-router-dom";
-import {connect} from "react-redux";
-import FlashCardsAction from "../redux/actions/FlashCardsGetActions";
-import CardFront from "./CardFront";
 
 class NavBar extends Component {
     constructor(props) {
         super(props)
         this.state = null
     }
+
+    logOut = () => {
+        localStorage.clear()
+        alert("You are signing out")
+        window.location.reload()
+      }
 
     render() {
         return (
@@ -21,6 +24,9 @@ class NavBar extends Component {
           </Menu.Item>
           <Menu.Item>
             <Link to="/cards"> Cards </Link>
+        </Menu.Item>
+        <Menu.Item>
+             <p onClick={() => this.logOut()}> Logout </p>
         </Menu.Item>
         </Menu>
       </div>
